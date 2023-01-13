84a6c22583995e58ec4ff7c331b3c905103eb864
William Hittle <williamhittle@mail.weber.edu>
2023-01-12 18:59:02 -0700

    i'm trying to set up my lab_01 python file, a comments file, and a lab_01 testing python file

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,15 +1,1 @@
-# the pound symbol is how to make comments
-#
-# let's print hello world, or a variation therof
-print("hello there")
-# cool!  note that Python lets you use single or double quotes, but you have to use the same to start/end.
-print('hi back')
-# when I start typing, it fills in the second bit
-# this can help if I want to use a ' somewhere
-print("Hey, it's a new world")
-# which would break if I typed 'Hey, it's a new world'
-# a multi-line statement can be done with 3 double quotes:
-print("""Hello there.
-How are you doing?
-I'm fine, thank you very much.
-""")
+# check play to see the working notes
