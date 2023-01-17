74a336d09b57314264ed160bd61b262a55d88be2
William Hittle <williamhittle@mail.weber.edu>
2023-01-16 17:38:18 -0700

    lab 1 set up progresses!

--- Lab 01 - First Program/lab_01 testing space.py
+++ Lab 01 - First Program/lab_01 testing space.py
@@ -23,3 +23,20 @@ I'm fine, thank you very much.
 print   ("almost fine, but editing isn't the best")
 print("got it!")
 # all of that should work fine
+
+print("Note the subsequent print statements show up in the \"Run\" window with a blank line between them.")
+print("Also note, I have to use an escape code to make this \" appear")
+print("""There are escape codes for single quotes like \',
+double quotes like \" (seen above),
+and also for things lik a tab that can be used to intent a line
+\tlike this.""")
+print("""Be careful when using\\r, because if you type it in the middle of a line of code and keep writing,
+it'll print everything after the \\r from the starting position,
+which can overwrite stuff.
+Watch as this line is erased. \\r by this 
+when I leave the \\r in as a carriage return
+Watch as this line is erased. \r by this
+""")
+print("""You probably meant to use \\n,
+which puts a new line in\nwhile all the code\nstays on the same line.
+""")
--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,22 @@
-# check play to see the working notes
-# lab_01 testing space.py should have all the code I want to eventually put here, but with comments
+print("I like to use print statements with triple \" marks.")
+print("""
+When I do that, I like to write my code with the \"\"\" before and after my block of text,
+but note that doing so adds extra spaces to the output,
+because Python is counting those \"returns\" as keystrokes that need to get printed.
+""")
+print("Also note, I have to use an escape code to make this \" appear")
+print("""There are escape codes for single quotes like \',
+double quotes like \" (seen above),
+and also for things lik a tab that can be used to indent a line
+\tlike this.""")
+print("""Be careful when using \\r, because if you type it in the middle of a line of code and keep writing,
+it'll print everything after the \\r from the starting position,
+which can overwrite stuff.
+Watch as this line is erased. \\r by this 
+when I leave the \\r in as a carriage return.
+Watch as this line is erased. \r by this
+""")
+print("""You probably meant to use \\n,
+which puts a new line in\nwhile all the code\ncan stay on the same line.
+Or just type things in as a new line with the triple-double-quotes, \"\"\".
+""")
